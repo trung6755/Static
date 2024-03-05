f7143794d9bc810858fe04867cc503b2a08f8160
trung6755 <trungggrr@gmail.com>
2024-03-05 15:53:25 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student.Change();
+            Student studenA = new Student(100, "Goku");
+
+            
+            studenA.Display();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Static
+{
+    public class Student
+    {
+        private int id;
+        private string name;
+        private static string uni = "HUST";
+
+        public Student(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public static void Change()
+        {
+            uni = "USTH";
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(id + " " + name + " " + uni);
+        }
+    }
+}
